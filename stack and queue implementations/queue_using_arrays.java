a281b24faf7fe873425fd15c0620283fc9a69fc4
BasuKallapur <basukallapur0@gmail.com>
2025-03-30 16:47:17 +0530

    added files

--- stack and queue implementations/queue_using_arrays.java
+++ stack and queue implementations/queue_using_arrays.java
@@ -4,6 +4,7 @@ class Queue{
     int start= -1;
     int end= -1;
     int currSize= 0;
+
     // push
     void push(int x){
         if(currSize == maxSize) {
@@ -18,6 +19,7 @@ class Queue{
         q[end]= x;
         currSize++;
     }
+
     // pop
     int pop(){
         int popEle= q[start];
@@ -35,6 +37,7 @@ class Queue{
         currSize--;
         return popEle;
     }
+
     // top
     int top(){
         if(currSize == 0) {
@@ -43,17 +46,37 @@ class Queue{
         }
         return q[start];
     }
+
     // size
     int size(){
         return currSize;
     }
-    // print
+
+    // // print
+    // @Override
+    // public String toString() {
+    //     if (currSize == 0) return "Queue is empty";
+
+    //     StringBuilder sb = new StringBuilder();
+        
+    //     for (int i = start; ; i = (i + 1) % maxSize) {
+    //         sb.append(q[i]).append(" ");
+    //         if (i == end) break;  // Stop when we reach the last inserted element
+    //     }
+
+    //     return sb.toString();
+    // }
+
+    // or
     @Override
     public String toString(){
         if(currSize == 0) return "Queue is empty";
         StringBuilder sb= new StringBuilder();
-        for(int i=start; i<=end; i++){
-            sb.append(q[i] + " ");
+        int i= start;
+        while (true) {
+            sb.append(q[i]).append(" ");
+            if(i == end) break;
+            i= (i + 1) % maxSize;
         }
         return sb.toString();
     }
